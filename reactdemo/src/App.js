817ff2aa2a2bdc4ba5a55f7178ded0fe4b4fe57e
Prathapgithub1 <prathapkunarapu1@gmail.com>
2024-08-01 21:48:57 +0530

    first commit

--- reactdemo/src/App.js
+++ reactdemo/src/App.js
@@ -1,51 +1,76 @@
-// import logo from './logo.svg';
-// import './App.css';
+// // import logo from './logo.svg';
+// // import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+// // function App() {
+// //   return (
+// //     <div className="App">
+// //       <header className="App-header">
+// //         <img src={logo} className="App-logo" alt="logo" />
+// //         <p>
+// //           Edit <code>src/App.js</code> and save to reload.
+// //         </p>
+// //         <a
+// //           className="App-link"
+// //           href="https://reactjs.org"
+// //           target="_blank"
+// //           rel="noopener noreferrer"
+// //         >
+// //           Learn React
+// //         </a>
+// //       </header>
+// //     </div>
+// //   );
+// // }
 
-// export default App;
+// // export default App;
 
-import React from "react";
-
-//functional Component
+// import React from "react";
+// //functional Component
 // let App = () => {
+//     let s1 = {
+//         backgroundColor: "blue",
+//         color: "black"
+//     }
 //     return (
 //         <div>
-//             <h1>Hello Prathap</h1>
+//             <h1 style={s1}>Hello Prathap</h1>
 //         </div>
 //     )
 // }
 
+//export default App
+
+// class App extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <ul>
+//                     <li>UserName</li>
+//                     <li>Prathap</li>
+//                 </ul>
+//             </div>
+//         )
+//     }
+// }
+
 // export default App
 
-class App extends React.Component {
+import Home from './Home'
+import React, { Component } from 'react'
+
+class App extends Component {
     render() {
+        let Names = ["prathap", "raju", "sampath", "Kraju", "ERaju"]
         return (
-            <div>
-                <ul>
-                    <li>UserName</li>
-                    <li>Prathap</li>
-                </ul>
-            </div>
+            <>
+                {
+                    Names.map(
+                        (n) => {
+                            return <Home user={n} />
+                        }
+                    )
+                }
+            </>
         )
     }
 }
---
+++ reactdemo/src/Home.js
@@ -0,0 +1,11 @@
+
+function Home(props) {
+    // console.log(props)
+    return (
+        <>
+            <h1 >My Name is {props.user}</h1>
+        </>
+    )
+}
+
+export default Home
